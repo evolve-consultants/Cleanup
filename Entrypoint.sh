da651f711a5638566385663f4c982b68f2d024df
adamwillett123 <31134982+adamwillett123@users.noreply.github.com>
2018-09-11 11:17:27 +0100

    Update Entrypoint.sh

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -12,7 +12,8 @@ fi
 
 #Bring down review app stacks
 for stack in '$RANCHER_CLI stacks ls -q | grep review-'; do
-  $RANCHER_CLI rm -s $stack
+  #$RANCHER_CLI rm -s $stack
+  echo "deleting $stack"
   
 
 		
